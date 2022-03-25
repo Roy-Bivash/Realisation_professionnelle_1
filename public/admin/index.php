d1990e0afe1d56b7e01a8d36d6c9301a57159201
Roy-Bivash <82892496+Roy-Bivash@users.noreply.github.com>
2022-03-25 18:58:46 +0100

    Add files via upload

---
+++ public/admin/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Facture</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="../../css/style.css">
+    </head>
+    <body>
+        <br><br>
+        <div class="container">
+            <div class="titre">
+                <h1>Administrateur</h1>
+                <hr>
+                <br><br>
+                <h5 class="text-center fw-lighter">Pour accéder à votre compte administrateur utilisez l'application windows</h5>
+                <br>
+                <!-- <a href="/" download="application.zip" type="button" class="btn btn-outline-dark">Télécharger l'application</a> -->
+                <a href="application.zip" download="application.zip" type="button" class="btn btn-outline-dark">Télécharger l'application</a>
+            </div>
+
+        </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+    </body>
+</html>
